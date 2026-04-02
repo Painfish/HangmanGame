3ae2545576d07f2d0afd2850f375f8e53e021e51
Painfish <115902157+Painfish@users.noreply.github.com>
2026-04-01 22:52:27 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ let image        = document.getElementById('hangmanImg');
 let guessedWord  = document.getElementById('guessedWord');
 let wordHint     = document.getElementById('hint');
 let keyboardBtns = document.getElementById('keyboard');
-let keys         = document.getElementsByClassName('keys')
+let keys         = document.getElementsByClassName('keys');
 let newGame      = document.getElementById('newGame');
 
 let wordToGuess = '';
@@ -39,7 +39,7 @@ keyboardBtns.innerHTML += keyboard();
 let index = 0;
 while (index < keys.length){
     keys[index].addEventListener('click', function(){
-        if (wordToGuess.includes(this.innerText)){
+       if (wordToGuess.includes(this.innerText)){
             if (guessedLetters.includes(this.innerText)) {
                 console.log("letter already guessed.")
             }
@@ -52,3 +52,22 @@ while (index < keys.length){
     })
     index ++;
 }
+newGame.addEventListener('click', function() {
+    fetch("words.json")
+        .then(function(response){
+            if( response.ok ){
+                return response.json();
+            }
+            else {
+                console.log("Network error: fetch failed");
+            }
+        })
+        .then(function(data){
+            let setLen = data.length;
+            let randNum = Math.floor(Math.random() * setLen);
+            wordToGuess = data[randNum].word;
+            hangmanObj = new Hangman(wordToGuess);
+            guessedWord.innerHTML = hangmanObj.returnhtml();
+            wordHint.innerText = data[randNum].hint;
+        });  
+})
